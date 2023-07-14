d64e097b11e0c457a932ec043482b53d915d6106
Ryan Henrique <ryanhardflip@gmail.com>
2023-07-14 14:04:21 -0300

    feat

--- app/ThemeSwitcher.js
+++ app/ThemeSwitcher.js
@@ -4,7 +4,7 @@ import { useTheme } from "next-themes"
 
 const ThemeSwithcer = () => {
   const [mounted, setMounted] = useState(false)
-  const {theme, setTheme} = useTheme();
+  const {theme, setTheme} = useTheme("light");
 
   useEffect(() => {
     setMounted(true)
@@ -15,8 +15,8 @@ const ThemeSwithcer = () => {
   }
     return(
       <div className='w-fit h-fit rounded-sm cursor-pointer mb-4'>   
-          {theme ==="dark" && <button className='tracking-wide border-2 border-white px-2' onClick={() => setTheme('light')}>LIGHT</button>}
-          {theme ==="light" &&  <button className='text-zinc-700 tracking-wider border-2 border-zinc-700 px-2' onClick={() => setTheme('dark')}>DARK</button>}   
+         <button className='tracking-wide border-2 border-white px-2' onClick={() => setTheme('light')}>LIGHT</button>
+         <button className='text-zinc-700 tracking-wider border-2 border-zinc-700 px-2' onClick={() => setTheme('dark')}>DARK</button>   
       </div>
     )
 }

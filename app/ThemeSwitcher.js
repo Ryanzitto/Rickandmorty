f679395e4ea37dc0586dcf83499ae3bf2a14dc9d
Ryan Henrique <ryanhardflip@gmail.com>
2023-07-14 14:12:03 -0300

    feat

--- app/ThemeSwitcher.js
+++ app/ThemeSwitcher.js
@@ -15,8 +15,8 @@ const ThemeSwithcer = () => {
   }
     return(
       <div className='w-fit h-fit rounded-sm cursor-pointer mb-4'>   
-         <button className='tracking-wide border-2 border-white px-2' onClick={() => setTheme('light')}>LIGHT</button>
-         <button className='text-zinc-700 tracking-wider border-2 border-zinc-700 px-2' onClick={() => setTheme('dark')}>DARK</button>   
+          {theme ==="dark" && <button className='tracking-wide border-2 border-white px-2' onClick={() => setTheme('light')}>LIGHT</button>}
+          {theme ==="light" &&  <button className='text-zinc-700 tracking-wider border-2 border-zinc-700 px-2' onClick={() => setTheme('dark')}>DARK</button>}   
       </div>
     )
 }
